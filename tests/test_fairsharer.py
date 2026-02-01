8dfb76b8221b23b305d98b17eb7fe39b1c9be04d
Elin <Paiman.ibrahim@study.hs-duesseldorf.de>
2026-02-01 13:42:39 +0100

    Fix import path for pytest in CI

--- tests/test_fairsharer.py
+++ tests/test_fairsharer.py
@@ -1,5 +1,8 @@
-from fairsharer.fairsharer import fair_sharer
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
+from fairsharer.fairsharer import fair_sharer
 
 def test_fair_sharer_one_iteration():
     values = [0, 1000, 800, 0]
@@ -9,3 +12,4 @@ def test_fair_sharer_one_iteration():
 def test_fair_sharer_two_iterations():
     values = [0, 1000, 800, 0]
     assert fair_sharer(values, 2) == [100, 890, 720, 90]
+
